bb658f8a38c9db668c84a3d189f7e87be023868c
shadowbq <shadowbq@users.noreply.github.com>
2021-10-21 12:45:12 -0400

    Update setup-secrets-ubuntu.sh

--- installers/setup-secrets-ubuntu.sh
+++ installers/setup-secrets-ubuntu.sh
@@ -5,9 +5,11 @@ gpg --list-keys
 sudo update-alternatives --install /usr/bin/pinentry pinentry /usr/bin/pinentry-tty 200
 gpg-connect-agent reloadagent /bye
 curl -O -J -L "https://raw.githubusercontent.com/shadowbq/matrix.secrets/main/bash/.matrix.secrets" > ~/.matrix.secrets
+echo 'Load in this session: "source ~/.matrix.secrets"'
 echo 'source ~/.matrix.secrets' >> ~/.bashrc
+export GPG_TTY=$(tty)
 echo 'export GPG_TTY=$(tty)' >> ~/.bashrc
-echo '# Load an Existing Foreign GPG Key'
+echo '# Run these commands to Load an Existing Foreign GPG Key'
 echo 'export keypath="./my-gpg-private-key.asc"'
 echo 'export fingerprint=$(gpg --quiet --import-options import-show --import $keypath | sed -e "2!d" -e "s/^[ \t]*//")'
 echo 'echo "${fingerprint}:6:" | gpg --import-ownertrust'
